cb513bfe1f5f4e07930bebf082c847e88b8514d8
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-13 16:48:42 +0100

    Structure ticket custom + js structure and test for the form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const priceForm = document.getElementById('price-form');
+
 const nameAndSurname = document.getElementById('nomecognome');
 
 console.log(nameAndSurname);
@@ -7,3 +9,23 @@ const kmDaPercorrere = document.getElementById('sceltakm');
 const etàScelta = document.getElementById('selectetà');
 
 const buttonGenera = document.getElementById('generabtn');
+
+const ticketPreviewContainer = document.getElementById('ticket-preview');
+
+//Prezzo per Km
+const priceForKm = 0.21;
+
+//Calcolo
+const priceTicket = kmDaPercorrere * priceForKm;
+
+//Dati value campi
+const nameAndSurnameCompiled = nameAndSurname.value;
+
+const kilometriCompiled = kmDaPercorrere.value;
+
+const etàSelected = etàScelta.value;
+
+priceForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('Evento avvenuto pagina non ricaricata');
+});
