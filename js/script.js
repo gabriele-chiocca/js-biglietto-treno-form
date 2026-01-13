bbc39460813c684562d698fed674f18788c6bdb2
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-13 17:18:28 +0100

    structure js with function and const, complete

--- js/script.js
+++ js/script.js
@@ -12,20 +12,49 @@ const buttonGenera = document.getElementById('generabtn');
 
 const ticketPreviewContainer = document.getElementById('ticket-preview');
 
+//Preview
+const namePreview = document.getElementById('nameinsert');
+
+const pricePreview = document.getElementById('pricecalc');
+
 //Prezzo per Km
 const priceForKm = 0.21;
 
-//Calcolo
-const priceTicket = kmDaPercorrere * priceForKm;
-
-//Dati value campi
-const nameAndSurnameCompiled = nameAndSurname.value;
-
-const kilometriCompiled = kmDaPercorrere.value;
-
-const etàSelected = etàScelta.value;
+//Function
 
 priceForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log('Evento avvenuto pagina non ricaricata');
+
+  const nameAndSurnameCompiled = nameAndSurname.value;
+
+  const kilometriCompiled = kmDaPercorrere.value;
+
+  const etàSelected = etàScelta.value;
+
+  //Prende il nome e cognome inserito dall'utente
+  namePreview.innerText = nameAndSurnameCompiled;
+
+  let outputPrezzo;
+
+  if (etàSelected == 'Maggiorenne') {
+    ticketPreviewContainer.classList.remove('d-none');
+
+    const priceTicket = kilometriCompiled * priceForKm;
+
+    outputPrezzo = priceTicket - priceTicket * 0.4;
+
+    pricePreview.innerText = outputPrezzo.toFixed(2);
+  } else if (etàSelected == 'Minorenne') {
+    ticketPreviewContainer.classList.remove('d-none');
+
+    const priceTicket = kilometriCompiled * priceForKm;
+
+    outputPrezzo = priceTicket - priceTicket * 0.2;
+
+    pricePreview.innerText = outputPrezzo.toFixed(2);
+  } else {
+    console.error('Ricontrolla input inseriti');
+    ticketPreviewContainer.classList.add('d-none');
+    return;
+  }
 });
