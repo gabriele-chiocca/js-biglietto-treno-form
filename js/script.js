c57435ac51b2d13c411dde5a1bc93d159eea616c
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-13 15:25:29 +0100

    base js const

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 const nameAndSurname = document.getElementById('nomecognome');
+
+console.log(nameAndSurname);
+
+const kmDaPercorrere = document.getElementById('sceltakm');
+
+const etàScelta = document.getElementById('selectetà');
+
+const buttonGenera = document.getElementById('generabtn');
